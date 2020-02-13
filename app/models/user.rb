67815672b70426b0bfe24dac2f31a88820a662fc
Yuhei Kikuta <conan0226.yuu@gmail.com>
2020-02-13 13:03:22 +0900

    add address model

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,10 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  validates :nickname, presence: true, uniqueness: true
+  validates :email, uniqueness: true
+  has_one :address
+  has_many :pays
+  has_many :exibitions
+  has_one :purchase
 end
--- db/migrate/20200213034444_devise_create_users.rb
+++ db/migrate/20200213034444_devise_create_users.rb
@@ -4,8 +4,17 @@ class DeviseCreateUsers < ActiveRecord::Migration[5.2]
   def change
     create_table :users do |t|
       ## Database authenticatable
+      t.string :nickname,           null: false, 
       t.string :email,              null: false, default: ""
       t.string :encrypted_password, null: false, default: ""
+      t.string :family_name
+      t.string :family_name
+      t.string :last_name
+      t.string :j_family_name
+      t.string :j_last_name
+      t.integer :birthday_year
+      t.integer :birthday_month
+      t.integer :birthday_day
 
       ## Recoverable
       t.string   :reset_password_token
@@ -35,7 +44,7 @@ class DeviseCreateUsers < ActiveRecord::Migration[5.2]
 
       t.timestamps null: false
     end
-
+    add_index :users, :nickname,             unique: true
     add_index :users, :email,                unique: true
     add_index :users, :reset_password_token, unique: true
     # add_index :users, :confirmation_token,   unique: true
