c7fd104287a5a82c3347f39f21786eb23afc0b24
okiku-stt <59636022+okiku-stt@users.noreply.github.com>
2020-02-14 17:22:06 +0900

    Userテーブル、addressモデルのカラムの追加、バリデーションの追加 (#25)
    * change authenticate
    
    * delete ds store
    
    * add user-model varidation
    
    * change user and address model
    
    * modify readme
    
    * change from birthday to birth
    
    * update schema file

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,9 +3,34 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-  validates :nickname, presence: true, uniqueness: true
-  validates :email, uniqueness: true
+  validates :nickname, presence: true
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  validates :email, {presence: true, format: { with: VALID_EMAIL_REGEX },uniqueness: { case_sensitive: false }}
+  validates :family_name, presence: true,
+            format: {
+              with: /[^ -~｡-ﾟ]+/,
+              message: "全角のみで入力して下さい"
+            }
+  validates :last_name, presence: true,
+            format: {
+              with: /[^ -~｡-ﾟ]+/,
+              message: "全角のみで入力して下さい"
+            }
+  validates :j_family_name, presence: true,
+            format: {
+              with: /[^ -~｡-ﾟ]+/,
+              message: "全角のみで入力して下さい"
+            }
+  validates :j_last_name, presence: true,
+            format: {
+              with: /[^ -~｡-ﾟ]+/,
+              message: "全角のみで入力して下さい"
+            }
+  validates :birth_year, presence: true
+  validates :birth_month, presence: true
+  validates :birth_day, presence: true
   has_one :address
+  accepts_nested_attributes_for :address
   has_many :pays
   has_many :exibitions
   has_one :purchase
--- db/migrate/20200213034444_devise_create_users.rb
+++ db/migrate/20200213034444_devise_create_users.rb
@@ -7,14 +7,13 @@ class DeviseCreateUsers < ActiveRecord::Migration[5.2]
       t.string :nickname,           null: false
       t.string :email,              null: false, default: ""
       t.string :encrypted_password, null: false, default: ""
-      t.string :family_name
-      t.string :family_name
-      t.string :last_name
-      t.string :j_family_name
-      t.string :j_last_name
-      t.integer :birthday_year
-      t.integer :birthday_month
-      t.integer :birthday_day
+      t.string :family_name,        null: false
+      t.string :last_name,          null: false
+      t.string :j_family_name,      null: false
+      t.string :j_last_name,        null: false
+      t.integer :birth_year,     null: false
+      t.integer :birth_month,    null: false
+      t.integer :birth_day,      null: false
 
       ## Recoverable
       t.string   :reset_password_token
@@ -44,7 +43,6 @@ class DeviseCreateUsers < ActiveRecord::Migration[5.2]
 
       t.timestamps null: false
     end
-    add_index :users, :nickname,             unique: true
     add_index :users, :email,                unique: true
     add_index :users, :reset_password_token, unique: true
     # add_index :users, :confirmation_token,   unique: true
