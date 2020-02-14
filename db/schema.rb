979d8c002f34b44b0cf246d3f43bb735a63e34fe
Yuhei Kikuta <conan0226.yuu@gmail.com>
2020-02-14 16:33:59 +0900

    update schema file

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,32 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_02_13_031211) do
+ActiveRecord::Schema.define(version: 2020_02_13_085155) do
+
+  create_table "addresses", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.integer "user_id", null: false
+    t.string "ship_family_name", null: false
+    t.string "ship_last_name", null: false
+    t.string "ship_j_family_name", null: false
+    t.string "ship_j_last_name", null: false
+    t.integer "postal_code", null: false
+    t.string "prefectures", null: false
+    t.string "municipalities", null: false
+    t.string "house_number", null: false
+    t.string "building"
+    t.integer "phone_number"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "cards", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.integer "card_number", null: false
+    t.integer "year", null: false
+    t.integer "month", null: false
+    t.integer "security_number", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "exhibitions", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.integer "user_id", null: false
@@ -22,6 +47,26 @@ ActiveRecord::Schema.define(version: 2020_02_13_031211) do
     t.integer "price", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "nickname", null: false
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "family_name", null: false
+    t.string "last_name", null: false
+    t.string "j_family_name", null: false
+    t.string "j_last_name", null: false
+    t.integer "birth_year", null: false
+    t.integer "birth_month", null: false
+    t.integer "birth_day", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
 end
