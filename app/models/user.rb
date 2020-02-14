09794f883576d282f85aa241ca1ce60257ca3412
Yuhei Kikuta <conan0226.yuu@gmail.com>
2020-02-14 16:02:03 +0900

    add user-model varidation

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,9 +3,16 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-  validates :nickname, presence: true, uniqueness: true
-  validates :email, uniqueness: true
+  validates :nickname, presence: true,
+            format: {
+              with: /[^ -~｡-ﾟ]+/,
+              message: "全角のみで入力して下さい"
+            }
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  validates :email, {presence: true, format: { with: VALID_EMAIL_REGEX },uniqueness: { case_sensitive: false }}
+
   has_one :address
+  accepts_nested_attributes_for :address
   has_many :pays
   has_many :exibitions
   has_one :purchase
