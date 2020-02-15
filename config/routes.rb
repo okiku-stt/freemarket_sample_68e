7c823d420c79af543e6681cd67cba4b089071183
okiku-stt <59636022+okiku-stt@users.noreply.github.com>
2020-02-15 09:51:52 +0900

    トップページのリンクとマイページのビュー (#29)
    * top-page set link
    
    * restore mypage index

--- config/routes.rb
+++ config/routes.rb
@@ -1,17 +1,12 @@
 Rails.application.routes.draw do
 
   devise_for :users
-  
-  get 'cards/index'
-  get 'cards/new'
-  get 'cards/show'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: "top#index"
-  resources :top ,only: :index
   resources :mypage ,only: :index
   resources :logouts ,only: :index
   resources :paymethods ,only: :index
   resources :addresses ,only: :index
   resources :editmails ,only: :index
   resources :sell ,only: :index
+  resources :cards ,only: [:index,:new,:show]
 end
