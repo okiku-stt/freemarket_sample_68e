05c53035e2fcfd7370e2a7180dc88da72fb09a12
naoya-miyashita198805 <naoya.miyashita1988@gmail.com>
2020-02-18 13:29:20 +0900

    [WIP]pay.jp実装
    cardコントローラー
    new pay delete show の記載まで

--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,12 @@ Rails.application.routes.draw do
   resources :addresses ,only: :index
   resources :editmails ,only: :index
   resources :sell ,only: :index
-  resources :cards ,only: [:index, :new, :create, :show]
-
+  # resources :cards ,only: [:index, :new, :create, :show]
+  resources :card, only: [:new, :show] do
+    collection do
+      post 'show', to: 'card#show'
+      post 'pay', to: 'card#pay'
+      post 'delete', to: 'card#delete'
+    end
+  end
 end
