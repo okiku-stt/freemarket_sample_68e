f5aef6fe24cb8eeb6731347a7714c7d53408c7ed
Yuhei Kikuta <conan0226.yuu@gmail.com>
2020-02-18 11:30:20 +0900

    comment out other tests

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,9 +1,16 @@
 FactoryBot.define do
   factory :user do
-      nickname              {"abe"}
-      email                 {"kkk@gmail.com"}
+      nickname              {"いくちゃん"}
+      email                 {"erika.ikuta.0122@gmail.com"}
       password              {"00000000"}
       password_confirmation {"00000000"}
+      family_name {"生田"}
+      last_name {"絵梨花"}
+      j_family_name {"いくた"}
+      j_last_name {"えりか"}
+      birth_year {"1996"}
+      birth_month {"1"}
+      birth_day {"22"}
     
   end
 end
