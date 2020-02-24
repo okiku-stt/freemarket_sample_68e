df633440c14f6525872064a4a279395659121500
txkxzxwx <shun.619@gmail.com>
2020-02-24 16:08:12 +0900

    Revert "Merge branch 'takazawa-activehash-branch' of https://github.com/okiku-stt/freemarket_sample_68e into takazawa-activehash-branch"
    This reverts commit dbecf68755ac1aa87bcbda1e4be7a4b80cc502c3, reversing
    changes made to 304d241cedb482b829680414316be3b5785aba6a.

--- app/controllers/exhibitions_controller.rb
+++ app/controllers/exhibitions_controller.rb
@@ -1,16 +1,11 @@
 class ExhibitionsController < ApplicationController
 
-  before_action :set_exhibition, only: [:show, :edit]
-  before_action :set_user, only: [:show, :edit]
-
-
-
+  before_action :set_exhibition, only: [:show]
   def index
     @exhibitions = Exhibition.all
   end
-
+  
   def new
-
     @exhibition = Exhibition.new
   end
   
@@ -26,14 +21,13 @@ class ExhibitionsController < ApplicationController
     
   end
 
-  def show
+  def modal
     
   end
 
-  def create
-
+  def show
+  
   end
-
 
   def edit
 
@@ -44,13 +38,8 @@ class ExhibitionsController < ApplicationController
     params.require(:exhibition).permit(:price,:shipping_date,:shipping_charges,:categorys_name,:item_description,:item_status, :item_name, :prefecture_id).merge(user_id: current_user.id)
   end
 
-
   def set_exhibition
     @exhibition = Exhibition.find(params[:id])
-  end
-
-  def set_user
-    @user = User.find(params[:id])
   end
 
 end
