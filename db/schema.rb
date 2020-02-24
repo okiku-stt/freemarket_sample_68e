a635eab530ec3167c41b075b2dfba156d3bff47e
txkxzxwx <shun.619@gmail.com>
2020-02-24 16:30:33 +0900

    takazawa-uglifier-コメントアウト

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_02_19_092046) do
+ActiveRecord::Schema.define(version: 2020_02_24_024811) do
 
   create_table "addresses", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.integer "user_id", null: false
@@ -36,9 +36,17 @@ ActiveRecord::Schema.define(version: 2020_02_19_092046) do
     t.datetime "updated_at", null: false
   end
 
+  create_table "categories", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.string "ancestry"
+    t.index ["ancestry"], name: "index_categories_on_ancestry"
+  end
+
   create_table "exhibitions", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.integer "user_id", null: false
-    t.string "categorys_name", null: false
+    t.integer "category_id", null: false
     t.string "shipping_charges", null: false
     t.integer "prefecture_id", null: false
     t.string "shipping_date", null: false
