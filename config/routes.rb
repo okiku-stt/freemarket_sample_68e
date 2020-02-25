f0f1b5e44ce764a3fd6b27233cf61f5c2f369396
Yuhei Kikuta <conan0226.yuu@gmail.com>
2020-02-25 19:06:13 +0900

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -6,15 +6,15 @@ Rails.application.routes.draw do
   }
   root to: "exhibitions#index"
 
-
-
-  resources :exhibitions ,only: [:index, :show, :edit, :new, :create, :update]
+  resources :exhibitions ,only: [:index, :show, :edit, :new, :create, :update] do
     resources :categories do
       collection do
         get 'modal'
         get 'search_children'
         get 'search_grandchildren'
       end
+    end
+  end
 
   resources :exhibitions ,only: [:index, :show, :new, :create, :edit, :update] do
     collection do
@@ -23,16 +23,7 @@ Rails.application.routes.draw do
       get 'category_grandchildren'
     end
   end
-
-  resources :categories do
-    collection do
-      get 'search_children'
-      get 'search_grandchildren'
-
-    end
-  end
-  
-
+ 
   resources :mypage ,only: :index
   resources :logouts ,only: :index
   resources :paymethods ,only: :index
@@ -40,9 +31,7 @@ Rails.application.routes.draw do
   resources :editmails ,only: :index
   
   resources :cards, only: [:new, :index, :show, :edit, :update] do
-
     collection do
-      # post 'index', to: 'cards#index'
       post 'show', to: 'cards#show'
       post 'pay', to: 'cards#pay'
       post 'delete', to: 'cards#delete'
