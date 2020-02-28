974664a8f208f6a0597c11345c15238289be816d
Yuhei Kikuta <conan0226.yuu@gmail.com>
2020-02-28 18:09:02 +0900

    config image view

--- app/models/exhibition.rb
+++ app/models/exhibition.rb
@@ -5,14 +5,7 @@ class Exhibition < ApplicationRecord
   belongs_to :category
   has_many :images, dependent: :destroy
   accepts_nested_attributes_for :images, allow_destroy: true
-  
-  # belongs_to :purchase
-  # belongs_to_active_hash :prefecture
-
-class Exhibition < ApplicationRecord
-  extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to_active_hash :prefecture
-end
 
   validates :price, presence: true
   validates :shipping_date, presence: true
@@ -20,7 +13,7 @@ end
   def self.search(search)
     if search
       Exhibition.where('item_name  LIKE(?)',"%#{search}%")
-                            # ↑これがExhibitionテーブルにある該当するカラム名を引っ張ってこれる！
+    # これがExhibitionテーブルにある該当するカラム名を引っ張ってこれる！
     else
       Exhibition.all
     end
