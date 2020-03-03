4c4f4950825d79faee8503551ff1844fed202fd4
gusya33 <v6qx8nnc3@gmail.com>
2020-03-03 12:45:11 +0900

    商品詳細マークアップ-add

--- config/routes.rb
+++ config/routes.rb
@@ -14,7 +14,7 @@ Rails.application.routes.draw do
   end
   # resources :categories doに7つのアクションがないと指摘を受けました。対応をお願いします  
 
-  resources :exhibitions ,except: :destroy do
+  resources :exhibitions do
     collection do
       get 'modal'
       get 'search'
