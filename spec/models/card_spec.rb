47270b3a3ab2b8bd71cdda37aaed9a26a8a3320a
naoya-miyashita198805 <naoya.miyashita1988@gmail.com>
2020-03-04 18:35:07 +0900

    指摘事項修正

--- spec/models/card_spec.rb
+++ spec/models/card_spec.rb
@@ -2,17 +2,6 @@ require 'rails_helper'
 
 describe Card do
   describe '#pay' do
-    # it "expectの引数にしたインスタンスが全てのバルデーションをクリアした場合" do
-    #   card = create(:card)
-    #   expect(card).to be_valid?
-    # end
-
-    # it "userがログインしていなければ登録できない" do
-    #   user = create(:user)
-    #     # ↑ここに入れてみる 
-    #   allow(Payjp::Charge).to receive(:create).and_return(PayjpMock.prepare_valid_charge)
-    #   expect(card).to be_valid?
-    # end
 
     it "card_idがないと登録できない" do
       card = build(:card, card_id: nil)
@@ -26,9 +15,5 @@ describe Card do
       expect(card.errors[:customer_id]).to include()
     end
 
-    # it "有効期限の月が空欄の場合は登録できない" do
-    #   card = build(:card, mounth: nil)
-    #   card.be_valid?
-    # end
   end
 end
